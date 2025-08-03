690f6a7dbd76aa3d921a5933c6df8367f2d9cb3e
MahmoudAteef <100537916+MahmoudAteef@users.noreply.github.com>
2025-08-03 10:47:37 +0300

    Add files via upload

---
+++ app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Contact } from './components/contact/contact';
+
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Home' },
+  { path: 'about', component: About, title: 'About' },
+  { path: 'portfolio', component: Portfolio, title: 'Portfolio' },
+  { path: 'contact', component: Contact, title: 'Contact' },
+];
